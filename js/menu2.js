dc4aa71611497c957b4463bbe0a98cb75da79de6
unalejandrosanchez <70336808+unalejandrosanchez@users.noreply.github.com>
2020-08-27 11:28:50 -0500

    Add files via upload

---
+++ js/menu2.js
@@ -0,0 +1,7 @@
+var btnmenu = document.getElementById("btn-menu");
+var nav = document.getElementById("nav");
+btnmenu.addEventListener('click', function() {
+    
+    nav.classList.toggle ('mostrar');
+    
+})
